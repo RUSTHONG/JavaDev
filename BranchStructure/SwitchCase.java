ff1722f6b5764371b276be3663f82ea3be759c45
RUSTHONG <1456774060@qq.com>
2018-03-25 20:42:00 +0800

    second commit

--- BranchStructure/SwitchCase.java
+++ BranchStructure/SwitchCase.java
@@ -1,3 +1,17 @@
+/**
+ * 执行流程：
+ *      当程序运行到switch-case结构，直接利用switch之后的变量去case中做匹配，找到
+ *      完全匹配的case选项，执行case之后的处理方式，如果没有任何的一个case匹配执行,
+ *      default里面的最终处理方式
+ * 
+ * 注意事项
+ *      1. 在switch-case结构中，运行的代码只能是在case到break之间的代码！！！
+ *      2. break可以省略，但是之前的case会继续往下执行，直到遇到break跳出switch-case结构
+ *      3. 在case之外的代码是不会被运行的，在java中这个会被认为是一个无效代码，如果是eclipse提示
+ *      是一个unreachable code
+ *      4. 在switch-case 中不能出现相同的case选项
+ *      5. default 可省略
+ */
 import java.util.Scanner;
 
 class SwitchCase {
